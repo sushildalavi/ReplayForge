12386681983bd148c3c915f01dd44b7fd3c719b4
sushildalavi <75267758+sushildalavi@users.noreply.github.com>
2026-05-25 13:23:16 -0700

    Recovery: integrate XAUTOCLAIM execution loop to capture unacknowledged logs

--- worker/pkg/recovery/janitor.go
+++ worker/pkg/recovery/janitor.go
@@ -20,3 +20,33 @@ func StartJanitor(ctx context.Context, rdb *redis.Client, interval time.Duration
 		}
 	}
 }
+
+func AutoClaimPending(ctx context.Context, rdb *redis.Client, streamName, groupName, consumerName string, minIdle time.Duration) ([]redis.XMessage, error) {
+	claimed := make([]redis.XMessage, 0)
+	start := "0-0"
+
+	for {
+		msgs, nextStart, err := rdb.XAutoClaim(ctx, &redis.XAutoClaimArgs{
+			Stream:   streamName,
+			Group:    groupName,
+			Consumer: consumerName,
+			MinIdle:  minIdle,
+			Start:    start,
+			Count:    50,
+		}).Result()
+		if err != nil {
+			if err == redis.Nil {
+				return claimed, nil
+			}
+			return claimed, err
+		}
+
+		claimed = append(claimed, msgs...)
+		if len(msgs) == 0 || nextStart == "0-0" {
+			break
+		}
+		start = nextStart
+	}
+
+	return claimed, nil
+}
